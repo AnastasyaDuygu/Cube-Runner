b8480d9f25bf6f3593eee3d6c47b78b01e110e92
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-16 16:15:25 +0300

    (script) add : TutorialCanvas script to TutorialCanvas, has UnityEvent of onLevelStart

---
+++ Assets/[Cube Runner]/Scripts/TutorialCanvas.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace adk
+{
+    public class TutorialCanvas : MonoBehaviour
+    {
+        [SerializeField] private Button tapToPlayButton;
+    
+        public UnityEvent onLevelStart;
+
+        private void OnEnable()
+        {
+            tapToPlayButton.onClick.AddListener(OnStartButtonClicked);
+        }
+        private void OnDisable()
+        {
+            tapToPlayButton.onClick.RemoveAllListeners();
+        }
+        private void OnStartButtonClicked()
+        {
+            onLevelStart.Invoke();
+        }
+
+        public void DisableTapToPlayPanel()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
