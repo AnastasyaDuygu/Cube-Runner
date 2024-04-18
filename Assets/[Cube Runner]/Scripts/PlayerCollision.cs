320fadf083cf519ece0bc9f0c6ada93e204527c6
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-18 13:28:05 +0300

    (script) update : PlayerCollision script, added coin collision detection

--- Assets/[Cube Runner]/Scripts/PlayerCollision.cs
+++ Assets/[Cube Runner]/Scripts/PlayerCollision.cs
@@ -1,19 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace adk
 {
-    [RequireComponent(typeof(Rigidbody))]
     public class PlayerCollision : MonoBehaviour
     {
-        private Rigidbody rb;
         public UnityEvent onObstacleHit;
-        private void OnEnable()
-        {
-            rb = GetComponent<Rigidbody>();
-        }
+        private Vector3 endPos;
         private void OnCollisionEnter(Collision collision)
         {
             if(collision.collider.tag == "Obstacle")
@@ -22,6 +15,18 @@ namespace adk
                 //player can move false
                 //enable restart canvas
             } 
+        }
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(other.gameObject.tag == "Coin")
+            {
+                //Add Coin Collected Event
+                //make coin jump
+                    //endPos = other.transform.position;
+                    //other.gameObject.transform.DOJump(endPos, jumpPower, jumpCount, duration);
+                //coin image animation (DOTween)
+                //coin text jump (DOTween)
+            }
         }
     }
 }
