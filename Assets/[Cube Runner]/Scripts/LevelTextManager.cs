3ec72bc9e3a70ed7cd061160fc06e1a8097efad6
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-18 16:11:27 +0300

    (script) update : LevelTextManager script, now level text changes according to scene index

--- Assets/[Cube Runner]/Scripts/LevelTextManager.cs
+++ Assets/[Cube Runner]/Scripts/LevelTextManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,16 +6,18 @@ namespace adk
 {
     public class LevelTextManager : MonoBehaviour
     {
-        public float currentLvlNo = 0;
+        [SerializeField] private TextMeshProUGUI levelNoText;
+        private float currentLvlNo = 0;
         private void Awake()
         {
             currentLvlNo = SceneManager.GetActiveScene().buildIndex + 1;
             //Debug.Log(currentLvlNo);
+            levelNoText.text = "Level " + currentLvlNo;
         }
-
-        public void UpdateLevelNo()
+        
+        /*public void UpdateLevelNo()
         {
             currentLvlNo = SceneManager.GetActiveScene().buildIndex + 1;
-        }
+        }*/
     }
 }
