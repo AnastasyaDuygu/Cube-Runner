e6e0c2e128fc72b9471833a215de068076d9d8ae
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-19 10:47:49 +0300

    (script) add : LevelManagerEditor script, added a button to LevelManager to load next level

---
+++ Assets/[Cube Runner]/Scripts/Editor Scripts/LevelManagerEditor.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace adk
+{
+    [CustomEditor(typeof(LevelManager))]
+    public class LevelManagerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            LevelManager myScript = (LevelManager)target;
+            if (GUILayout.Button("Load Next level"))
+            {
+                myScript.LoadNextLevel();
+            }
+        }
+    }
+}
+
